f4040f9eb0d4a493f87f76853c6cb3d03699203c
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-06-20 03:39:05 -0400

    created app and used Ionicons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -9,9 +10,7 @@ const instructions = Platform.select({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <Ionicons name="ios-pizza" color="red" size={100}/>
     </View>
   );
 }
