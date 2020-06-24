d2170761b73fe49dc133cdbe1625497645bfcdf9
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-06-24 03:07:54 -0400

    made style changes to the buttons and reset component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
+import {View, TouchableOpacity, Text, StyleSheet, Platform} from 'react-native';
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers';
 import FitSlider from '../components/FitSlider';
 import FitStepper from '../components/FitStepper';
@@ -9,13 +9,14 @@ import TextButton from '../components/TextButton';
 import { submitEntry, removeEntry } from '../utils/api';
 import { connect } from 'react-redux';
 import { addEntry } from '../actions';
+import { white, purple } from '../utils/colors';
 
 function SubmitBtn({onPress}){
     return (
-        <TouchableOpacity
+        <TouchableOpacity style={Platform.OS == 'ios' ? styles.iosSubmitBtn :styles.androidSubmitBtn}
             onPress={onPress}
         >
-            <Text>SUBMIT</Text>
+            <Text style={ styles.submitText}>SUBMIT</Text>
         </TouchableOpacity>
     )
 }
@@ -93,32 +94,28 @@ class AddEntry extends Component{
         const metaInfo = getMetricMetaInfo();
         if(this.props.alreadyLogged){
             return(
-                <View>
+                <View style={styles.center}>
                     <Ionicons
-                        name="ios-happy"
+                        name={Platform.OS == 'ios' ? 'ios-happy':'md-happy'}
                         size={100}
                     />
                     <Text>You already logged your info for today!</Text>
                     
                     <TextButton onPress={this.reset}>
-                        <Text>Reset</Text>
+                        <Text style={{padding: 10}}>Reset</Text>
                     </TextButton>
                 </View>
             )
 
         }
         return(
-            <View>  
-                <Text>Hello</Text>
-                <Text>Hello</Text>
-                <Text>Hello</Text>
+            <View style={styles.container}>
                 <DateHeader date={new Date().toLocaleDateString()} />
-        <Text>{JSON.stringify(this.state)}</Text>
                 {Object.keys(metaInfo).map((key)=>{
                     const {getIcon ,type , ...rest} = metaInfo[key];
                     const value = this.state[key];
                     return(
-                        <View key={key}>
+                        <View key={key} style={styles.row}>
                             {getIcon()}
                             {type === 'slider'
                                 ? <FitSlider
@@ -141,6 +138,52 @@ class AddEntry extends Component{
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20,
+        paddingTop:40,
+        backgroundColor:white,
+    },
+    row:{
+        flexDirection:'row',
+        flex:1,
+        alignItems:'center',
+    },
+    iosSubmitBtn:{
+        backgroundColor:purple,
+        padding:10,
+        borderRadius:7,
+        height:45,
+        marginLeft:40,
+        marginRight:40,
+    },
+    androidSubmitBtn:{
+        backgroundColor: purple,
+        padding: 10,
+        paddingLeft:30,
+        paddingRight:30,
+        borderRadius: 2,
+        height: 45,
+        alignSelf:'flex-end',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    submitText:{
+        color:white,
+        fontSize:22,
+        textAlign:'center'
+    },
+    center:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft:30,
+        marginRight:30,
+    }
+});
+
 function mapStateToProps (state){
     const key = timeToString();
 
